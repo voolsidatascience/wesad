1c1e2b6b87e1903e60356f1e2b5266a3d59e8649
Phuong Nguyen <ngthianhphuong@gmail.com>
2020-09-23 10:56:01 +0200

    LSTM notebook and h5 model

---
+++ wesad/wesad_experiments-master/demo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+################################################################
+# demo.sh
+################################################################
+BASEDIR=$(dirname $0)
+
+/bin/echo -e "Starting Demo!\nby Brady Sheehan"
+
+python3 ${BASEDIR}/src/main/Demo.py
